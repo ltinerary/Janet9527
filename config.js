4888200b7ab97b21a25ebb279d6e24f8606b598f
fan <fan24min@qq.com>
2023-07-12 11:10:52 +0800

    🍱add images
    ( •̀ ω •́ )y

--- config.js
+++ config.js
@@ -14,18 +14,30 @@ module.exports = {
   location: '101210301',
   type: '1,2,3,5,7,9,13,15,16',
   startDay: '2022-07-09',
-  imgs: ['https://img1.imgtp.com/2023/03/22/YHtQFzIB.jpg',
+  imgs: [
+    'https://img1.imgtp.com/2023/03/22/YHtQFzIB.jpg',
+    'https://img1.imgtp.com/2023/07/12/0cPNJVaH.jpg',
     'https://img1.imgtp.com/2023/04/06/wiEa0W72.jpg',
+    'https://img1.imgtp.com/2023/07/12/31jGJmW6.jpg',
     'https://img1.imgtp.com/2023/03/29/w0wUGEYZ.jpg',
+    'https://img1.imgtp.com/2023/07/12/HOFMwIB6.jpg',
     'https://img1.imgtp.com/2023/03/29/Kmr4fgI5.jpg',
+    'https://img1.imgtp.com/2023/07/12/jBLaLmPF.jpg',
     'https://img1.imgtp.com/2023/04/06/Gqd2kHMj.jpg',
+    'https://img1.imgtp.com/2023/07/12/8xwQ5yv8.jpg',
     'https://img1.imgtp.com/2023/03/29/mo1LihyA.jpg',
+    'https://img1.imgtp.com/2023/07/12/FI5MVf8j.gif',
     'https://img1.imgtp.com/2023/03/27/SEkfy2Ut.jpg',
     'https://img1.imgtp.com/2023/04/06/qBorUQ7K.jpg',
+    'https://img1.imgtp.com/2023/07/12/cxRsyJtG.jpg',
     'https://img1.imgtp.com/2023/03/29/1Zsj1URC.jpg',
+    'https://img1.imgtp.com/2023/07/12/IIGAioJe.jpg',
     'https://img1.imgtp.com/2023/03/27/RY6X8Dcw.png',
     'https://img1.imgtp.com/2023/05/22/7biVzDGx.jpeg',
+    'https://img1.imgtp.com/2023/07/12/a6E5b6I3.jpg',
     'https://img1.imgtp.com/2023/05/22/X7JxgQO9.jpeg',
-    'https://img1.imgtp.com/2023/05/22/t6gk6DXg.jpeg'
+    'https://img1.imgtp.com/2023/07/12/i1L16KQg.jpg',
+    'https://img1.imgtp.com/2023/05/22/t6gk6DXg.jpeg',
+    'https://img1.imgtp.com/2023/07/12/HwtV68nM.jpeg'
   ],//随机选取一张图片 todo
 }
